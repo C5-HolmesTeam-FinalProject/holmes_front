38f5f4d557c19d46b95db3b2ea1906e4a8fc0028
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-10-25 15:00:33 -0600

    fixing commits

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import '../assets/styles/card.scss';
 import card from '../assets/images/hotel1.jpg';
 import heart from '../assets/images/corazon.png';
@@ -25,33 +24,3 @@ const Card = ({ title }) => {
 };
 
 export default Card;
-=======
-import '../styles/card.css';
-
-const Card = () => {
-    return (
-        <section className="card-container">
-<article className="c-card">
-    <header className="c-card__header">
-        <img src="./img/hotel1.jpg" className="c-card__image" alt="Imagen de la Card" />
-        
-    </header>
-    <div className="c-card__body">
-        <h2 className="c-card__title">
-            <p>Hotel Chevere</p>
-            <img src="../img/corazon.png" className="c-card__corazon" alt="corazon" />
-        </h2>
-    </div>
-    
-    
-    <footer className="c-card__footer">
-        <img src="../img/cama.png" className="c-card__cama" alt="Recamaras" />
-        <img src="../img/bañera.png" className="c-card__bañera" alt="Baños" />
-        <img src="../img/metros.png" className="c-card__metros" alt="metros cuadrados" />
-
-    </footer>
-</article>
-</section>
-    )
-}
->>>>>>> 8d744bc11714ec9036555670b2a3b98793c178ba
