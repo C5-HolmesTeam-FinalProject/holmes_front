ae82b4c1c44502aa58706e799c9017df38244adf
acrux95 <67486473+ArmandoCloud@users.noreply.github.com>
2020-10-25 12:08:13 -0600

    merging card components

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
-import Select from '../components/Select';
+
+import Layout from '../components/Layout';
+import ButtonMenu from '../components/ButtonMenu';
 
 const AddPost = () => (
   <>
-    { Select }
+    <Layout>
+      <h1>Añadir Publicación</h1>
+      <div className="homeDescriptions">
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+      </div>
+      <div className="homeDescriptions2">
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+        <ButtonMenu />
+      </div>
+    </Layout>
   </>
 );
 
