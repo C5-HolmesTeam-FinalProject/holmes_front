f7ba1a80d108337b65a2b0d6cd0e2591f62651b8
jose caicedo <joseluiscl@gmail.com>
2020-10-26 01:45:03 -0500

    fix styles delete duplicated files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-
 import Cover from '../components/Cover';
 import Information from '../components/Information';
 import Search from '../components/Search';
 import Card from '../components/card';
+import Footer from '../components/Footer';
 
 const Home = () => (
-
   <>
     <Cover />
     <Search />
     <Information title='Trabaja con nosotros' />
     <Card title='Hotel Luxury' />
+    <Footer title='Haz tus sueños una realidad' />
   </>
 );
 
