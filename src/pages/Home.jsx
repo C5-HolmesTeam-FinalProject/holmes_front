bca3bdf1d9eccd5fcda75a559106a6c3131d2738
jose caicedo <joseluiscl@gmail.com>
2020-10-28 09:25:09 -0500

    add Features component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Cover from '../components/Cover';
 import Information from '../components/Information';
 import Search from '../components/Search';
-import Card from '../components/card';
 import Footer from '../components/Footer';
 import SimilarListings from '../components/SimilarListings';
 
