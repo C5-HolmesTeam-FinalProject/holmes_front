0b44d7108083ba09e4bd265fb6ad7fba83f71ab4
jose caicedo <joseluiscl@gmail.com>
2020-10-28 09:24:15 -0500

    add DetailsProperty component

---
+++ src/pages/DetailsProperty.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Carousel from '../components/Carousel';
+import SimilarListings from '../components/SimilarListings';
+import DetailBar from '../components/DetailsBar';
+import Description from '../components/Description';
+import Features from '../components/Features';
+
+const DetailsProperty = () => (
+  <>
+    <Header />
+    <Carousel />
+    <DetailBar />
+    <Description text='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' />
+    <Features feature='Sala de Juegos' />
+    <SimilarListings />
+    <Footer title='Haz tus sueños una realidad' />
+  </>
+);
+
+export default DetailsProperty;
